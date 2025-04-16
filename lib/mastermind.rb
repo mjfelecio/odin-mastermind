46703fe983b4cdd56d96efe095ff29ea36ba6a26
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-04-16 15:22:12 +0800

    Implement running the game in main.rb

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -2,4 +2,18 @@
 
 # Class that handles the logic for the game
 class Mastermind
+  def initialize
+  end
+
+  def start_game
+    puts 'Welcome to Mastermind'
+    puts 'Colors:
+      R (Red)
+      O (Orange)
+      Y (Yelow)
+      G (Green)
+      B (Blue)
+      V (Violet)'
+    puts 'Type \'M\' to be the Code Maker and \'B\' to be the Code Breaker'
+  end
 end
---
+++ main.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: false
+
+require_relative 'lib/mastermind'
+
+Mastermind.new.start_game
