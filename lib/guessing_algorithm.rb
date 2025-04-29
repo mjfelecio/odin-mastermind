ade46f272f72ce6fb0b5ea7c0b139a0dbeae9ff5
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-04-29 17:19:23 +0800

    Create function to filter valid secret codes

--- lib/guessing_algorithm.rb
+++ lib/guessing_algorithm.rb
@@ -13,4 +13,9 @@ class GuessingAlgoritm
   def generate_all_codes
     (1111..6666).to_a.select { |num| num.to_s.chars.all? { |c| c.to_i.between?(1, 6) } }
   end
+
+  def filter_valid_codes(codes, prev_guess, feedback)
+    # Filter the codes that would give the same feedback if they WERE the secret code
+    codes.select { |code| process(code, prev_guess) == feedback }
+  end
 end
