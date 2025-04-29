c2f223d7e6f0141c4f3bc886034b9669c067cad3
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-04-29 17:14:19 +0800

    Refactor pin checking to their own methods

--- lib/feedback_processor.rb
+++ lib/feedback_processor.rb
@@ -2,30 +2,35 @@
 
 module FeedbackProcessor
   def process(secret_code, guess)
-    feedback = []
-    code_chars = secret_code.to_s.chars
-    guess_chars = guess.to_s.chars
+    @feedback = []
+    @code_chars = secret_code.to_s.chars
+    @guess_chars = guess.to_s.chars
 
-    # Check for red pins
-    code_chars.each_with_index do |color, idx|
-      next unless guess_chars[idx] == color
+    check_red_pins
+    check_white_pins
 
-      feedback << '🔴'
-      code_chars[idx] = nil
-      guess_chars[idx] = nil
+    @feedback.join
+  end
+
+  def check_red_pins
+    @code_chars.each_with_index do |color, idx|
+      next unless @guess_chars[idx] == color
+
+      @feedback << '🔴'
+      @code_chars[idx] = nil
+      @guess_chars[idx] = nil
     end
+  end
 
-    # Check for white pins
-    guess_chars.each do |color|
+  def check_white_pins
+    @guess_chars.each do |color|
       next if color.nil?
-      next unless code_chars.include?(color)
+      next unless @code_chars.include?(color)
 
-      feedback << '⚪'
-      code_idx = guess_chars.find_index(color)
-      code_chars[code_idx] = nil
-      guess_chars[code_idx] = nil
+      @feedback << '⚪'
+      code_idx = @guess_chars.find_index(color)
+      @code_chars[code_idx] = nil
+      @guess_chars[code_idx] = nil
     end
-
-    feedback.join
   end
 end
