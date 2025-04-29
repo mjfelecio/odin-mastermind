2a9477e5452b4e63ccdbf26e74f397adc23351ad
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-04-29 17:17:34 +0800

    Create function to generate all code combinations

--- lib/guessing_algorithm.rb
+++ lib/guessing_algorithm.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: false
 
 class GuessingAlgoritm
+  include FeedbackProcessor
+
+  def initialize
+    @all_secret_code_combinations = generate_all_codes
+  end
+
+  def guess
+  end
+
+  def generate_all_codes
+    (1111..6666).to_a.select { |num| num.to_s.chars.all? { |c| c.to_i.between?(1, 6) } }
+  end
 end
